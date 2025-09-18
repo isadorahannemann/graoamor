21bd666dbfcad04faf26f742458016e10f2e6ad7
Isadora Hannemann <isadorahnm@gmail.com>
2025-09-18 16:57:46 -0300

    feat: index inicial

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8" />
+  <link rel="stylesheet" href="public/css/index.css">
+</head>
+<body>
+  <header>
+    <div class="logo">
+      <img src="public/images/folha.png" alt="Logo Grão de Amor" />
+      <h1>Grão de Amor</h1>
+    </div>
+    <nav role="navigation" aria-label="Menu principal">
+      <a href="index.php">Home</a>
+      <a href="sobre.php">Sobre</a>
+      <a href="acoes.php">Ações</a>
+      <a href="contato.php">Contato</a>
+    </nav>
+  </header>
+  <main>
+    <h2>Pequenos gestos para grandes transformações!</h2>
+    <p>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk.</p>
+  </main>
+
+  <footer>
+    &copy; 2025 Grão de Amor. Todos os direitos reservados.
+  </footer>
+</body>
+</html>
